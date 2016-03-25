21f47841c0872b040b89f56c1e6194e385886f32
Yura <yura61388@gmail.com>
2016-03-25 09:20:40 +0200

    start work with reports

--- app/admin/reports/reports.controller.js
+++ app/admin/reports/reports.controller.js
@@ -4,9 +4,34 @@
     angular.module("app.admin")
         .controller("ReportsController", ReportsController);
 
-    ReportsController.$inject = [];
+    ReportsController.$inject = ["reportsService"];
 
-    function ReportsController() {
+    function ReportsController(reportsService) {
         var vm = this;
+        vm.showReportsForm = showReportsForm;
+        vm.hideReportsForm = hideReportsForm;
+        vm.createReport = createReport;
+        vm.updateTestsByGroup = updateTestsByGroup;
+        activate();
+
+        function activate() {
+            
+        }
+
+        function showReportsForm() {
+            vm.reportsFormCollapsed = false;
+            vm.report = {};
+        }
+
+        function hideReportsForm() {
+            vm.reportsFormCollapsed = true;
+            vm.report = {};
+        }
+
+        function getGroups() {
+            reportsService.getGroups().then(function(data) {
+                vm.groupsList = Array.isArray(data) ?  data : [];
+            });
+        }
     }
 })();
--- app/services/reports.service.js
+++ app/services/reports.service.js
@@ -4,14 +4,33 @@
     angular.module("app.admin")
         .factory("reportsService", reportsService);
 
-    reportsService.$inject = [];
+    reportsService.$inject = ["$http", "BASE_URL", "URL", "groupsService"];
 
-    function reportsService() {
+    function reportsService($http, BASE_URL, URL, groupsService) {
         var service = {
+            getGroups: getGroups(),
+            getReport: getReport()
 
         };
 
         return service;
+
+        function _successCallback(response) {
+            return response.data;
+        }
+        function _errorCallback(response) {
+            return response;
+        }
+        
+        function getGroups() {
+            return groupsService.getGroups().then(_successCallback, _errorCallback);
+        }
+        
+        function getReport(group_id, test_id) {
+
+            
+            return "Report";
+        }
     }
 
 })();
