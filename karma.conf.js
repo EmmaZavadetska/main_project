f928d88592d15e6a193b406efe504cfd9f458232
Yura <yura61388@gmail.com>
2016-04-18 20:49:41 +0300

    fixed karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -3,17 +3,20 @@ module.exports = function(config) {
     config.set({
         basePath: "",
 
+        frameworks: ["jasmine"],
+
         files: ["./bower_components/angular/angular.js",
             "./node_modules/angular-mocks/angular-mocks.js",
-            ".tmp/js/app.js"],
+            ".tmp/js/app.js",
+            "./src/**/*.spec.js"],
 
         exclude: [],
 
-        plugins: ["karma-jasmine", "karma-chrome-launcher"],
+        plugins: ["karma-jasmine", "karma-chrome-launcher", "karma-htmlfile-reporter"],
 
         preprocessors: {},
 
-        reporters: ["progress", "karma-coverage", "karma-htmlfile-reporter"],
+        reporters: ["progress", "coverage", "html"],
 
         port: 9876,
 
