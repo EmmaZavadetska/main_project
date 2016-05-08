2785b9a899af0a7344fac42300e56d40ad35fcfc
EmmaZavadetska <emma.zavadetska@gmail.com>
2016-05-08 16:21:44 +0300

    add unit tests

--- src/app/admin/groups/students/students.controller.spec.js
+++ src/app/admin/groups/students/students.controller.spec.js
@@ -1,47 +1,95 @@
 'use strict';
 
 describe ("Test Students Controller", function () {
-    //beforeEach(angular.mock.module("app.admin.groups"));
-    //
-    //var ctrl;
-    //var _stateParams;
-    //var _studentsService;
-    //var _groupsService;
-    //var _customDialog;
-    //var _PAGINATION;
-    //var _MESSAGE;
-    //var _getHeadElements;
-    //
-    //beforeEach(angular.mock.inject(function($controller) {
-    //    var studentsService = {
-    //        getStudentsByGroupId: function () {},
-    //        getHeadElements: function () {},
-    //        addStudent: function () {},
-    //        removeStudent: function () {},
-    //        getStudentById: function () {},
-    //        editStudent: function () {}
-    //    };
-    //    var groupsService = {
-    //        getGroups: function () {}
-    //    };
-    //
-    //    spyOn(studentsService, "getStudentsByGroupId").and.returnValue("List of students by their id");
-    //    spyOn(groupsService, "getGroups").and.returnValue("list of groups");
-    //
-    //    ctrl = $controller("StudentsController", {
-    //        $stateParams: _stateParams,
-    //        studentsService: _studentsService,
-    //        getHeadElements: _getHeadElements,
-    //        groupsService: _groupsService,
-    //        customDialog: _customDialog,
-    //        PAGINATION: _PAGINATION,
-    //        MESSAGE:_MESSAGE
-    //    });
-    //}));
-    it("Should show the list of students", function(){
-        expect(true).toBe(true);
+    beforeEach(module("app.admin.groups"));
+    beforeEach(module("app"));
+
+    var controller;
+    var customDialog;
+
+    var studentsServiceMock = {};
+
+    var student = {
+        "user_id": "90",
+        "gradebook_id": "yudsdded",
+        "student_surname": "Соло",
+        "student_name": "Андрій",
+        "student_fname": "Іванович",
+        "group_id": "3",
+        "plain_password": "jo4xif529",
+        "photo": ""
+    };
+    studentsServiceMock.getHeadElements = function (){
+        return ["Ім'я", "Прізвище", "По-батькові", "Номер залікової книги", "Група"];
+    };
+
+
+    studentsServiceMock.getStudentsByGroupId = function (){
+        return [{
+            "user_id": "105",
+            "gradebook_id": "yudsddssdded",
+            "student_surname": "Солототототототототототот",
+            "student_name": "Андрій",
+            "student_fname": "Олегович",
+            "group_id": "11",
+            "plain_password": "jo4xi529",
+            "photo": ""
+        }, {
+            "user_id": "90",
+            "gradebook_id": "yudsdded",
+            "student_surname": "Соло",
+            "student_name": "Андрій",
+            "student_fname": "Іванович",
+            "group_id": "3",
+            "plain_password": "jo4xif529",
+            "photo": ""
+        }];
+    };
+
+    studentsServiceMock.removeStudent = function () {
+        var deferred = $q.defer();
+        deferred.resolve("Call");
+        return deferred.promise;
+    };
+
+    beforeEach(function(){
+        angular.module('test', ["app.admin.groups"]).value("studentsService", studentsServiceMock);
     });
-    it("Should show the list of students", function(){
-        expect(true).toBeTruthy(true);
+
+    beforeEach(inject(function ($controller, _customDialog_, $q) {
+        controller = $controller("StudentsController", {});
+        customDialog = _customDialog_;
+        spyOn(customDialog, "openDeleteDialog").and.callFake(function(){
+            var deferred = $q.defer();
+            deferred.resolve("Call fake promise");
+            return deferred.promise;
+        });
+        spyOn(customDialog, "openInformationDialog").and.callFake(function(){
+            var deferred = $q.defer();
+            deferred.resolve("Call fake promise");
+            return deferred.promise;
+        });
+    }));
+
+    beforeEach(function(){
+        spyOn(studentsServiceMock, "removeStudent");
+    });
+
+    it("max", function(){
+        expect(controller.maxSize).toBe(3);
+    });
+
+    it("get head elements", function(){
+        expect(controller.headElements).toEqual(["Ім'я", "Прізвище", "По-батькові", "Номер залікової книги", "Група"]);
+    });
+
+    beforeEach(function(){
+        controller.studentRemover(student);
+    },5000);
+
+    it("remove", function(){
+        expect(customDialog.openDeleteDialog).toHaveBeenCalled();
+        //expect(studentsServiceMock.removeStudent).toHaveBeenCalled();
+        //expect(studentsServiceMock.removeStudent).toHaveBeenCalledWith(44);
     });
 });
